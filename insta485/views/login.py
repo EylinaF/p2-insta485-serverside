50da7fcab257b5a71f6c9b8dffc09c60ac87bdf8
eylina <eylinaf@umich.edu>
2025-01-30 14:33:05 -0500

    login

---
+++ insta485/views/login.py
@@ -0,0 +1,60 @@
+"""
+Insta485 index (main) view.
+
+URLs include:
+/users/<user_url_slug>/following/
+"""
+import flask
+import insta485
+
+@insta485.app.route('/users/<user_url_slug>/following/')
+def show_following(user_url_slug):
+    """Display user following page."""
+
+    """
+    if 'username' not in flask.session:
+        return flask.redirect(flask.url_for('login'))
+
+    logname = flask.session['username']
+    """
+    logname = "awdeorio"
+
+    # Connect to database
+    connection = insta485.model.get_db()
+
+    cur = connection.execute(
+        "SELECT username FROM users WHERE username = ?",
+        (user_url_slug,)
+    )
+    user = cur.fetchone()
+
+    if user is None:
+        flask.abort(404)
+
+    cur = connection.execute(
+        """
+        SELECT users.username, users.filename AS user_img_url
+        FROM following
+        JOIN users ON following.username2 = users.username
+        WHERE following.username1 = ?
+        """,
+        (user_url_slug,)
+    )
+    following = cur.fetchall()
+
+    for follow in following:
+        follow["user_img_url"] = f"/uploads/{follow['user_img_url']}"
+        
+        cur = connection.execute(
+        "SELECT 1 FROM following WHERE username1 = ? AND username2 = ?",
+        (follow["username"], logname)
+        )
+        follow["logname_follows_username"] = cur.fetchone() is not None
+
+    context = {
+        "logname": logname,
+        "username": user_url_slug,
+        "following": following,
+    }
+
+    return flask.render_template("following.html", **context)
