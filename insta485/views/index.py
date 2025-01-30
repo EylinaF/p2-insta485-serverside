922f805ff824710b47dae9bdfa25cb9a234d778c
Mike Cai <mikecai@umich.edu>
2025-01-29 20:52:54 -0500

    change

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -8,23 +8,53 @@ import flask
 import insta485
 
 
-@insta485.app.route('/')
+@insta485.app.route("/")
 def show_index():
-    """Display / route."""
-
-        # Connect to database
+    """Display the index page with posts from the logged-in user and followed users."""
     connection = insta485.model.get_db()
-
-    # Query database
-    logname = "awdeorio"
-    cur = connection.execute(
-        "SELECT username, fullname "
-        "FROM users "
-        "WHERE username != ?",
-        (logname, )
-    )
-    users = cur.fetchall()
     
-    context = {"users": users}
-    return flask.render_template("index.html", **context)
+    # Hardcoded logged-in user for now, later replace with session
+    logname = "awdeorio"
 
+    # Get posts from the logged-in user and followed users
+    posts_query = """
+    SELECT posts.postid, posts.filename, posts.owner, posts.created,
+           users.filename AS owner_img
+    FROM posts
+    JOIN users ON posts.owner = users.username
+    WHERE posts.owner = ?
+       OR posts.owner IN (SELECT username2 FROM following WHERE username1 = ?)
+    ORDER BY posts.postid DESC;
+    """
+    
+    posts = connection.execute(posts_query, (logname, logname)).fetchall()
+
+    # Fetch likes and comments for each post
+    for post in posts:
+        postid = post["postid"]
+        
+        # Get like count
+        likes_query = "SELECT COUNT(*) AS likes FROM likes WHERE postid = ?"
+        likes = connection.execute(likes_query, (postid,)).fetchone()["likes"]
+
+        # Check if the logged-in user liked this post
+        user_liked_query = "SELECT 1 FROM likes WHERE postid = ? AND owner = ?"
+        user_liked = connection.execute(user_liked_query, (postid, logname)).fetchone()
+        post["liked"] = bool(user_liked)
+
+        # Get comments
+        comments_query = """
+        SELECT comments.commentid, comments.text, comments.owner
+        FROM comments
+        WHERE comments.postid = ?
+        ORDER BY comments.commentid ASC;
+        """
+        comments = connection.execute(comments_query, (postid,)).fetchall()
+
+        post["likes"] = likes
+        post["comments"] = comments
+
+        # Format timestamps using arrow for human-readable format
+        post["human_time"] = arrow.get(post["created"]).humanize()
+
+    return flask.render_template("index.html", logname=logname, posts=posts)
