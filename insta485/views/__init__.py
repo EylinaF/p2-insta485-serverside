ec5ce3402f58614537b3bdfeb3441116de37641d
Mike Cai <mikecai@umich.edu>
2025-01-30 13:54:17 -0500

    posts

--- insta485/views/__init__.py
+++ insta485/views/__init__.py
@@ -4,3 +4,4 @@ from insta485.views.uploads import get_uploaded_file
 from insta485.views.user import show_user_profile
 from insta485.views.followers import show_followers
 from insta485.views.following import show_following
+from insta485.views.posts import show_post
---
+++ insta485/views/posts.py
@@ -0,0 +1,63 @@
+import os
+import flask
+import insta485
+import arrow
+
+@insta485.app.route('/posts/<postid_url_slug>/')
+def show_post(postid_url_slug):
+    """Serve uploaded files only to authenticated users."""
+    
+    logname = "awdeorio"
+    #logname = flask.session.get("username")
+
+    connection = insta485.model.get_db()
+
+    cur = connection.execute(
+        """
+        SELECT posts.postid, posts.filename AS img_url, posts.created AS timestamp,
+               users.username AS owner, users.filename AS owner_img_url
+        FROM posts
+        JOIN users ON posts.owner = users.username
+        WHERE posts.postid = ?
+        """,
+        (postid_url_slug,)
+    )
+    post = cur.fetchone()
+
+    # If post doesn't exist, return 404
+    if post is None:
+        flask.abort(404)
+
+    cur = connection.execute(
+        "SELECT COUNT(*) AS like_count FROM likes WHERE postid = ?", (post["postid"],)
+    )
+    post["likes"] = cur.fetchone()["like_count"]
+
+    cur = connection.execute(
+        "SELECT 1 FROM likes WHERE postid = ? AND owner = ?", (post["postid"], logname)
+    )
+    post["liked_by_user"] = cur.fetchone() is not None
+
+    cur = connection.execute(
+        """
+        SELECT users.username AS owner, comments.text
+        FROM comments
+        JOIN users ON comments.owner = users.username
+        WHERE comments.postid = ?
+        ORDER BY comments.created ASC
+        """,
+        (post["postid"],)
+    )
+
+    post["comments"] = cur.fetchall()
+    post["img_url"] = f"/uploads/{post['img_url']}"
+    post["owner_img_url"] = f"/uploads/{post['owner_img_url']}"
+    post["timestamp"] = arrow.get(post["timestamp"]).humanize()
+
+    # Prepare context
+    context = {
+        "logname": logname,
+        "post": post,
+    }
+
+    return flask.render_template("post.html", **context)
