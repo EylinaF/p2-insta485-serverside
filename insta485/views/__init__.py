9afee42b0a44543bc99ab0b87020bbfb87b43697
eylina <eylinaf@umich.edu>
2025-01-30 15:04:21 -0500

    finish login

--- insta485/views/__init__.py
+++ insta485/views/__init__.py
@@ -6,3 +6,4 @@ from insta485.views.followers import show_followers
 from insta485.views.following import show_following
 from insta485.views.posts import show_post
 from insta485.views.explore import show_explore
+from insta485.views.login import show_login
