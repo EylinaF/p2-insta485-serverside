8c5d1efd8df89ac5b0ad4835e2cdadc43fe7d04a
eylina <eylinaf@umich.edu>
2025-01-30 22:43:28 -0500

    finish comments

--- insta485/views/comments.py
+++ insta485/views/comments.py
@@ -19,4 +19,38 @@ def handle_comments():
     """
 
     logname = "awdeorio"
+
+    connection = insta485.model.get_db()
+
+    operation = flask.request.form.get("operation")
+    target = flask.request.args.get("target", "/")
+    postid = flask.request.form.get("postid")
+    commentid = flask.request.form.get("commentid")
+    text = flask.request.form.get("text", "").strip()
+
+    if operation == "create":
+        if not text:
+            flask.abort(400)
+        
+        connection.execute(
+        "INSERT INTO comments (commentid, owner, postid, text) VALUES (?, ?, ?, ?)",
+        (commentid, logname, postid, text)
+        )
+    elif operation == "delete":
+        cur = connection.execute(
+        "SELECT owner FROM comments WHERE commentid = ?", (commentid,)
+        )
+        owner = cur.fetchone
+
+        """
+        if owner != logname:
+            flask.abort(403)
+        """
+        
+        
+        connection.execute("DELETE FROM comments WHERE commentid = ?", (commentid,))
+    
+    return flask.redirect(target)
+
+
     
