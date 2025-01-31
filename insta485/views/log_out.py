f661ed640df293db044f7f973d3dfcfd294952a5
eylina <eylinaf@umich.edu>
2025-01-31 09:32:05 -0500

    add session into logout

--- insta485/views/log_out.py
+++ insta485/views/log_out.py
@@ -10,9 +10,10 @@ import insta485
 @insta485.app.route('/logout/', methods=["POST"])
 def log_out():
    
-    
     """
     logout and return to login
     """
+
+    flask.session.clear()
     
     return flask.redirect("/accounts/login/")
